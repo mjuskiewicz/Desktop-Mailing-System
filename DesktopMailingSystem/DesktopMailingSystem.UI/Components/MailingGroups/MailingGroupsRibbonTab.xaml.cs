d35cb2463a6d9dac44111dc0ebc1f55c779f7ff1
mjuskiewicz <mikolaj.juskiewicz@gmail.com>
2016-08-29 21:32:33 +0200

    Changed the Shell view. Moved the ShellMenuRegion from Ribbon control to RibbonTab control. Added a example of Ribbon Group.

---
+++ DesktopMailingSystem/DesktopMailingSystem.UI/Components/MailingGroups/MailingGroupsRibbonTab.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls.Ribbon;
+
+namespace DesktopMailingSystem.UI.Components.MailingGroups
+{
+    /// <summary>
+    /// Interaction logic for MailingListRibbonTab.xaml
+    /// </summary>
+    public partial class MailingListRibbonTab : RibbonGroup
+    {
+        public MailingListRibbonTab()
+        {
+            InitializeComponent();
+        }
+    }
+}
--- DesktopMailingSystem/DesktopMailingSystem.UI/UIModule.cs
+++ DesktopMailingSystem/DesktopMailingSystem.UI/UIModule.cs
@@ -1,11 +1,22 @@
-﻿using Prism.Modularity;
+﻿using DesktopMailingSystem.Infrastructure;
+using Microsoft.Practices.Unity;
+using Prism.Modularity;
+using Prism.Regions;
 
 namespace DesktopMailingSystem.UI
 {
     public class UIModule : IModule
     {
+        IRegionViewRegistry _regionViewRegistry;
+
+        public UIModule(IUnityContainer container, IRegionViewRegistry regionViewRegistry)
+        {
+            _regionViewRegistry = regionViewRegistry;
+        }
+
         public void Initialize()
         {
+            _regionViewRegistry.RegisterViewWithRegion(RegionNames.ShellMenuRegion, typeof(Components.MailingGroups.MailingListRibbonTab));
         }
     }
 }
